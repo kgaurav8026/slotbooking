1270cc7af6671c8048b78cb0cc8d2879a232c3d5
Ritwik0710 <ritwikshri123@gmail.com>
2024-02-19 14:09:09 +0530

    ReBookings

--- src/app/components/Elements/Dropdown.jsx
+++ src/app/components/Elements/Dropdown.jsx
@@ -1,7 +1,7 @@
 function Dropdown(array, placeholder) {
   return (
     <div>
-      <select style = {{width : "500px"}}className="form-select" aria-label="Default select example">
+      <select className="form-select" aria-label="Default select example">
         <option selected disabled value="">{placeholder}</option>
         {array?.map((i) => (
           <option key={i} value={i}>
